62f1ac257d820aacdaa749bb82393763bc71700f
chanjoo <chanjoo@cjlee_47>
2020-05-28 17:07:39 +0900

    ...

--- WebContent/js/bpmn/instance/gen_instance.js
+++ WebContent/js/bpmn/instance/gen_instance.js
@@ -28,8 +28,43 @@ function genInstance(_entityId, _type,  _list_instance , _option ){
     this.containerId = "div_" + this.entityId + "_"+ this.type + "_" + this.idPrefix; 
     this.searchContainerId = this.containerId + "_searchContainer";
     this.gridCotainerId = this.containerId + "_gridContainer";
-    this.container = $("<div/>",{id:  this.containerId });
-    this.list_instance.container.append(this.container);
+    
+    var _this = this;
+    if(this.option != null && this.option.modal){
+        var modalCommon = $("[name=infiniteLogModal]");
+        this.modalClone = modalCommon.clone();
+        this.modalClone.attr("isCloned","true");
+
+        // Header
+        var filterStrs = [];
+        $.each(this.option.filter,function(k,v){
+            var str = _.camelCase(k);
+            str += " : " + v;
+            filterStrs.push(str);
+        });
+        var headStr = '[' + _.camelCase(_this.entityId) + ']  ' +  filterStrs.join(" , ");													
+        this.modalClone.find(".modal-header h6 span").text(headStr);
+
+        // modalId
+        this.modalClone.attr("id", this.containerId );
+        this.modalClone.attr("name", "infiniteLogModal" + this.containerId );
+        
+        this.container = this.modalClone.find(".modal-body");
+        this.list_instance.container.append(this.modalClone);
+
+        // modal
+        setTimeout( function(){
+            _this.modalClone.modal();
+            _this.modalClone.draggable({
+                handle: ".modal-header"
+            }); 
+        },0);
+        
+    }else{
+        this.container = $("<div/>",{id:  this.containerId });
+        this.list_instance.container.append(this.container);
+    }
+    
     this.searchContainer = null;
     this.gridCContainer = null;
     
@@ -52,6 +87,10 @@ function genInstance(_entityId, _type,  _list_instance , _option ){
     this.fn_contextmenu();
 
     $("#loader").hide();
+
+    if(this.option != null && this.option.modal){
+
+    }
 
 }
 
@@ -131,185 +170,136 @@ genInstance.prototype.fn_contextmenu = function(){
             "selector": "#" + _this.gridCotainerId + " .jqgrow td.contextMenu",
             "trigger": "left",
             "build": function($trigger) {
-                        var options = {
-                            callback: function(key, options) {
-                                // var m = "clicked: " + key;
-                                // window.console && console.log(m) || alert(m);
-                                options.items[key].callback.call(this,key,options);
-                            },
-                            items: {}
-            
-                        };
-            
-                        // if ($trigger.hasClass('something')) {
-                        // 	options.items.foo = {name: "foo"};
-                        // } else {
-                        //     options.items.bar = {name: "bar"};
-                        // }
-                        var columnName = $trigger.attr("columnName");
-                        if (columnName == null)
-                            return false;
-            
-                        var gridId = $trigger.attr("gridId");
-                        var grid = _this.container.find("#"+gridId).jqGrid();
-                        var rowId = $trigger.parent().attr("id");
-                        var rowData = grid.getRowData(rowId);
-                        var vGridOpt = grid.getGridParam();
-                        var cms = vGridOpt.colModel;
-                        var cm = _.find(cms, {name: $trigger.attr("columnName")} );
-                        // var models = eval(vGridOpt.modelVarName);
-                        // var vEntity = _.find([].concat(models.ownedEntities),{"_xmi:id": vGridOpt.entityId} );
-                        
-                        // parents
-                        var hasParents = false;
-                        var parents = [];
-                        if ( cm.referenceId != null){
+                var options = {
+                    callback: function(key, options) {
+                        // var m = "clicked: " + key;
+                        // window.console && console.log(m) || alert(m);
+                        options.items[key].callback.call(this,key,options);
+                    },
+                    items: {}
+    
+                };
+    
+                // if ($trigger.hasClass('something')) {
+                // 	options.items.foo = {name: "foo"};
+                // } else {
+                //     options.items.bar = {name: "bar"};
+                // }
+                var columnName = $trigger.attr("columnName");
+                if (columnName == null)
+                    return false;
+    
+                var gridId = $trigger.attr("gridId");
+                var grid = _this.container.find("#"+gridId).jqGrid();
+                var rowId = $trigger.parent().attr("id");
+                var rowData = grid.getRowData(rowId);
+                var vGridOpt = grid.getGridParam();
+                var cms = vGridOpt.colModel;
+                var cm = _.find(cms, {name: $trigger.attr("columnName")} );
+                // var models = eval(vGridOpt.modelVarName);
+                // var vEntity = _.find([].concat(models.ownedEntities),{"_xmi:id": vGridOpt.entityId} );
+                
+                // parents
+                var hasParents = false;
+                var parents = [];
+                if ( cm.referenceId != null){
+                    
+                    var src = _.find( _this.jpaFile.dataSrc, {"referenceId": cm.referenceId , "topRefrenceId" : cm.topRefrenceId  });
+                    // entytyId를 넣어야 하므로 camelCase 를 쓰지 않는다.
+                    var itemName = src.parentEntity;
+                    options.items[itemName] = {
+                        name: "Parents: " + itemName ,
+                        callback : function(key, options){
+                            var m = "clicked: " + key + ", data: " + $(this).text();
+    
+                            // Modal Pop
+                            if(true){
+                                // Form Submit
+                                var filter = {};
+                                $.each(src.childColumnNames , function(i,column){
+                                    var parentColumn = src.parentColumnNames[i];
+                                    if(rowData[column.toUpperCase()] != null)
+                                        filter[parentColumn.toUpperCase()] = rowData[column.toUpperCase()];
+                                    // $(this).attr("cellValue");
+                                });
+
+                                var instanceOption = {
+                                    modal : true,
+                                    caller : _this ,
+                                    filter : filter
+                                };
+                                _this.list_instance.add_instance ( itemName , 'general' , instanceOption );
+                                
+                            }
+                            return; 
+    
                             
-                            var src = _.find( _this.jpaFile.dataSrc, {"referenceId": cm.referenceId , "topRefrenceId" : cm.topRefrenceId  });
-                            // entytyId를 넣어야 하므로 camelCase 를 쓰지 않는다.
-                            var itemName = src.parentEntity;
-                            options.items[itemName] = {
-                                name: "Parents: " + itemName ,
-                                callback : function(key, options){
-                                    var m = "clicked: " + key + ", data: " + $(this).text();
-            
-                                    // Modal Pop
-                                    if(true){
-                                        var pageBody = _this.list_instance.container;
-                                        var modalCommon = $("[name=infiniteLogModal]");
-                                        // var modalClone = $(modalCommon.html());
-                                        var modalClone = modalCommon.clone();
-                                        pageBody.append(modalClone);
-                                        modalClone.attr("isCloned","true");
-            
-                                        // Form Submit
-                                        var filter = {};
-                                        $.each(src.childColumnNames , function(i,column){
-                                            var parentColumn = src.parentColumnNames[i];
-                                            if(rowData[column.toUpperCase()] != null)
-                                                filter[parentColumn.toUpperCase()] = rowData[column.toUpperCase()];
-                                            // $(this).attr("cellValue");
-                                        });
-
-                                        var instanceOption = {
-                                            isPopSelect : true,
-                                            caller : _this ,
-                                            filter : filter
-                                        };
-                                        _this.list_instance.add_instance ( itemName , 'general' , instanceOption );
-
-                                        
-                                        // Header
-                                        var filterStrs = [];
-                                        $.each(filter,function(k,v){
-                                            var str = _.camelCase(k);
-                                            str += " : " + v;
-                                            filterStrs.push(str);
-                                        });
-                                        var headStr = '[' + itemName + ']  ' +  filterStrs.join(" , ");													
-                                        modalClone.find(".modal-header h6 span").text(headStr);
-            
-                                        modalClone.modal();
-                                        // parent.$("#"+modalId).modal();
-                                        modalClone.draggable({
-                                            handle: ".modal-header"
-                                        }); 
-                                        
-                                        
-                                    }
-                                    return; 
-            
-                                    
-                                }
-                            };
-                            hasParents = true;
-                            // }
-            
                         }
-            
-                        // children
-                        var hasChildrens = false;
-                        var childrens = _this.jpaFile.childReferences;
-            
-                        $.each(childrens , function(i,child){
-                            var child_columns = child.child_columns;
-                            var parent_columns = child.parent_columns;
-                            var child_columns_index = _.indexOf(child.parent_columns, cm.name.toLowerCase());
-                            if (child_columns_index == -1)
-                                return false;
+                    };
+                    hasParents = true;
+                    // }
+    
+                }
+    
+                // children
+                var hasChildrens = false;
+                var childrens = _this.jpaFile.childReferences;
+    
+                $.each(childrens , function(i,child){
+                    var child_columns = child.child_columns;
+                    var parent_columns = child.parent_columns;
+                    var child_columns_index = _.indexOf(child.parent_columns, cm.name.toLowerCase());
+                    if (child_columns_index == -1)
+                        return false;
 
-                            // entytyId를 넣어야 하므로 camelCase 를 쓰지 않는다.
-                            var itemName = child.childEntityName;
-                            options.items[itemName] = {
-                                name: "Child: " + itemName ,
-                                callback : function(key, options){
-                                    // var m = "clicked: " + key + ", data: " + $(this).text();
-                                    // console.log(m);
-            
-                                    // Modal Pop
-                                    if(true){
-                                        var pageBody = _this.list_instance.container;
-                                        var modalCommon = $("[name=infiniteLogModal]");
-
-                                        var modalClone = modalCommon.clone();
-                                        pageBody.append(modalClone);
-                                        modalClone.attr("isCloned","true");
-                                        
-                                        // Form Submit
-                                        var filter = {};
-                                        $.each(child_columns , function(i,column){
-                                            var parentColumn = parent_columns[i];
-                                            if(rowData[column.toUpperCase()] != null)
-                                                filter[column.toUpperCase()] = rowData[parentColumn.toUpperCase()];
-                                            // $(this).attr("cellValue");
-                                        });
-                                        
-                                        var instanceOption = {
-                                            isPopSelect : true,
-                                            caller : _this ,
-                                            filter : filter
-                                        };
-                                        _this.list_instance.add_instance ( itemName , 'general' , instanceOption );
-                                        
-                                        // Header
-                                        var filterStrs = [];
-                                        $.each(filter,function(k,v){
-                                            var str = _.camelCase(k);
-                                            str += " : " + v;
-                                            filterStrs.push(str);
-                                        });
-                                        var headStr = '[' + itemName + ']  ' +  filterStrs.join(" , ");													
-                                        modalClone.find(".modal-header h6 span").text(headStr);
-            
-                                        modalClone.modal();
-                                        // parent.$("#"+modalId).modal();
-                                        modalClone.draggable({
-                                            handle: ".modal-header"
-                                        }); 
-                                        
-                                        
-                                    }
-                                    return ;
-            
-                                   
-                                }
-                            };
-                            hasChildrens = true;
+                    // entytyId를 넣어야 하므로 camelCase 를 쓰지 않는다.
+                    var itemName = child.childEntityName;
+                    options.items[itemName] = {
+                        name: "Child: " + itemName ,
+                        callback : function(key, options){
+                            // var m = "clicked: " + key + ", data: " + $(this).text();
+                            // console.log(m);
+    
+                            // Modal Pop
+                            if(true){
+                                // Form Submit
+                                var filter = {};
+                                $.each(child_columns , function(i,column){
+                                    var parentColumn = parent_columns[i];
+                                    if(rowData[column.toUpperCase()] != null)
+                                        filter[column.toUpperCase()] = rowData[parentColumn.toUpperCase()];
+                                    // $(this).attr("cellValue");
+                                });
+                                
+                                var instanceOption = {
+                                    modal : true,
+                                    caller : _this ,
+                                    filter : filter
+                                };
+                                _this.list_instance.add_instance ( itemName , 'general' , instanceOption );
+                                
+                            }
+                            return ;
+    
                             
-                            
-                        });
-                        
-                        if ( !hasParents && !hasChildrens )
-                            return false;
-            
-                        if ( _.keys(options.items).length == 1 ){
-                            options.items[_.keys(options.items)[0]].callback();
-                            return false;
                         }
-            
-                        return options;
-                        // return false; 
-                    }
+                    };
+                    hasChildrens = true;
+                    
+                    
+                });
+                
+                if ( !hasParents && !hasChildrens )
+                    return false;
+    
+                if ( _.keys(options.items).length == 1 ){
+                    options.items[_.keys(options.items)[0]].callback();
+                    return false;
+                }
+    
+                return options;
+                // return false; 
+            }
 
         }
     );
