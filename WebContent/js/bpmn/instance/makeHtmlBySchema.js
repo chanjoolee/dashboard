1149f2e1eb9536aae8e0dca95a7f2692df4eb0d9
chanjoo <chanjoo@DESKTOP-L1AB6JJ>
2021-04-21 23:41:28 +0900

    모달창 고정을 다시 다이나믹으로 원복함. 맨 마지막 모들테두리를 파란색으로

--- WebContent/js/bpmn/instance/makeHtmlBySchema.js
+++ WebContent/js/bpmn/instance/makeHtmlBySchema.js
@@ -921,12 +921,12 @@ makeHtmlBySchema.prototype.grid = function(_schema ,_schema_parent , container ,
             // gridBody.height( gridBody.height() + 10  );
 
             // 높이를 고정하므로 주석처리한다.
-            // var gridBody = $("#gbox_" + gridParam.id + " .ui-jqgrid-bdiv");
-            // gridBody.height( "100%");   
+            var gridBody = $("#gbox_" + gridParam.id + " .ui-jqgrid-bdiv");
+            gridBody.height( "100%");   
             if( !_this.instance.option.modal ){
                 grid.setGridWidth(_this.instance.container.width());
             }  
-            // gridBody.height( gridBody.height() + 10  );       
+            gridBody.height( gridBody.height() + 10  );       
             
                 
             
